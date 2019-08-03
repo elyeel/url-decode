06df9bc759b14b5268a215989c5e8db743c7f941
Risyardi Iswanto <elyeel@yahoo.com>
2019-08-03 06:44:38 +0000

    Final Version of urlDecoder

--- urlDecode.js
+++ urlDecode.js
@@ -1,11 +1,11 @@
 const urlDecode = function(text) {
   // Put your solution here
-  const result = separatePercent(separateEqual(separateAnd(text)));
+  const result = separateEqual(separateAnd(separatePercent(text)));
   return result;
-}
+};
 
 // separate '&' into array
-const separateAnd = function(string) { 
+const separateAnd = function(string) {
   let resultKeys = [];
   let keyTemp = "";
   for (let i = 0; i < string.length; i++) {
@@ -15,7 +15,7 @@ const separateAnd = function(string) {
     } else {
       keyTemp += string[i];
     }
-  };
+  }
   resultKeys.push(keyTemp);
   return resultKeys;
 };
@@ -25,6 +25,7 @@ const separateEqual = function(arr) { //split array into object
   let keyValue = '';
   let result = {};
   let flag = false;
+  let keys = [];
   for (let char of arr) {
     for (let i = 0; i < char.length; i++) {
       if (char[i] === '=') {
@@ -37,54 +38,36 @@ const separateEqual = function(arr) { //split array into object
         }
       }
     }
-    console.log(keyTemp + " = " + keyValue);
+    // console.log(keyTemp + " = " + keyValue);
     result[keyTemp] = keyValue;
+    keys.push(keyTemp);
     keyValue = '';
     keyTemp = '';
     flag = false;
   }
+  // return result; // result already an object
+  for (let char of keys) {
+    result[char] = separatePercent(result[char]);
+  }
+  // console.log(result);
   return result;
 };
 
 const separatePercent = function(string) {
   let result = '';
-  const key = string[Object.keys(string)];
-  for (let i = 0; i < key.length; i++) {
-    if (key[i] === "%") {
+  // const key = string[Object.keys(string)];
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "%") {
       i += 2;
       result += ' ';
     } else {
-      result += key[i];
+      result += string[i];
     }
   }
   return result;
 };
-  
-  
 
 console.log(urlDecode("duck=rubber"));
 console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
-// console.log(separateEqual(separateAnd("city=Vancouver&weather=lots%20of%20rain")));
-
-// if (string[i] === '&' || string[i] === (string.length - 1)) { //fetch data to result
-//   result['keyTemp'] = keyValue;
-//   keyTemp = ''; //reset temp to ''
-//   keyValue = '';
-// } else {
-  
-// }
-// for (let i = 0; i <= string.length; i++) {
-//   if (i === (string.length) || flag ) { //fetching data
-//     resultKeys.push(keyTemp);
-//     keyTemp = '';
-//     flag = false
-//   } else if (string[i] === '&') {
-//     flag = true;
-//     resultKeys.push(keyTemp);
-//     keyTemp = '';
-//   } else {
-//     keyTemp += string[i];
-//   }
-// }
